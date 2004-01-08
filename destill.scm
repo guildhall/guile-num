75c82d079ed7810b0358e3accbc601e7c3f7fd30
Arno W. Peters <a.w.peters@ieee.org>
2004-01-08 00:23:53 +0000

    *** empty log message ***

--- destill.scm
+++ destill.scm
@@ -26,4 +26,39 @@
 		   (else
 		    (postprocess #f file port))))))))
 
-(postprocess #f "/usr/include/sndfile.h" (current-input-port))
+(define program-name (car (command-line)))
+(define program-version "0.0.1")
+
+(define help
+  (string-append "Usage: " program-name " [OPTION] FILE
+
+Destill from standard input only those parts that have come from the
+macro-expanded FILE.
+
+  --help               This message
+  --version            Version information
+"))
+
+(define version
+  (string-append program-name " " program-version))
+
+(define *non-option-args* '())
+
+(for-each (lambda (s)
+	    (cond ((string=? s "--help")
+		   (format #t "~A\n" help)
+		   (quit))
+		  ((string=? s "--version")
+		   (format #t "~A\n" version)
+		   (quit))
+		  (else
+		   (set! *non-option-args* (cons s *non-option-args*)))))
+	  (cdr (command-line)))
+
+(case (length *non-option-args*)
+  ((0) (begin (format #t "~A\n" help)
+	      (quit))
+   (1) (postprocess #f (car *non-option-args*) (current-input-port))
+   (else (error "Only support one file argument"))))
+
+
